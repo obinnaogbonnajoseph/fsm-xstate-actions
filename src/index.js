0d1e6d94a53eee83f32661cab7c500852c6b747e
Obinna Ogbonna <oogbonna@gidimobile.com>
2020-09-02 03:45:55 +0100

    initialize npm
    implement fsm xstate actions

--- src/index.js
+++ src/index.js
@@ -3,46 +3,52 @@ import { createMachine, interpret } from 'xstate';
 const elBox = document.querySelector('#box');
 
 const setPoint = (context, event) => {
-  // Set the data-point attribute of `elBox`
-  // ...
+    // Set the data-point attribute of `elBox`
+    // ...
+    elBox.dataset.point = `(${event.clientX}, ${event.clientY})`
 };
 
 const machine = createMachine({
-  initial: 'idle',
-  states: {
-    idle: {
-      on: {
-        mousedown: {
-          // Add your action here
-          // ...
-          target: 'dragging',
+    initial: 'idle',
+    states: {
+        idle: {
+            on: {
+                mousedown: {
+                    // Add your action here
+                    // ...
+                    actions: setPoint,
+                    target: 'dragging',
+                },
+            },
         },
-      },
-    },
-    dragging: {
-      on: {
-        mouseup: {
-          target: 'idle',
+        dragging: {
+            on: {
+                mouseup: {
+                    target: 'idle',
+                },
+            },
         },
-      },
     },
-  },
+}, {
+    actions: {
+        setPoint
+    }
 });
 
 const service = interpret(machine);
 
 service.onTransition((state) => {
-  console.log(state);
+    console.log(state);
 
-  elBox.dataset.state = state.value;
+    elBox.dataset.state = state.value;
 });
 
 service.start();
 
 elBox.addEventListener('mousedown', (event) => {
-  service.send(event);
+    service.send(event);
 });
 
 elBox.addEventListener('mouseup', (event) => {
-  service.send(event);
+    service.send(event);
 });
